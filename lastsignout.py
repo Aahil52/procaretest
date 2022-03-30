a8edb39deb31c249f62e17a0303d277b0d113e37
Aahil Lakhani <lakhaniaahil@gmail.com>
2022-03-30 01:01:56 -0400

    add test script and test json responses

---
+++ lastsignout.py
@@ -0,0 +1,39 @@
+import json
+from datetime import datetime
+
+def get_last_sign_out(students_attendance):
+    sign_out_times = []
+    for i in range(len(students_attendance)):
+        sot_str = students_attendance[i]['sign_out_time']
+        sot_obj = datetime.strptime(sot_str, '%Y-%m-%d %H:%M')
+        sign_out_times.append((students_attendance[i]['student_id'], sot_obj))
+    sign_out_times.sort(key=lambda x: x[1], reverse = True)
+    return sign_out_times[0][0]
+
+def get_room_name(rooms, id):
+    for i in range(len(rooms)):
+        if rooms[i]['id'] == id:
+            return rooms[i]['name']
+
+sa = open('students_attendance.json')
+sdi = open('student_detailed_info.json')
+r = open('rooms.json')
+
+# Would be call to Students Attendance with school_id as parameter
+students_attendance = json.load(sa)['attendance']
+
+student_id = get_last_sign_out(students_attendance)
+print(student_id)
+
+# Would be call to Student Detailed Info with student_id and school_id as parameter
+student_detailed_info = json.load(sdi)['student']
+
+student_name = student_detailed_info['first_name'] + ' ' + student_detailed_info['last_name']
+room_id = student_detailed_info['room_id']
+
+# Would be call to Rooms with school_id as parameter
+rooms = json.load(r)['rooms']
+
+room_name = get_room_name(rooms, room_id)
+
+print(room_name + ": " + "Please bring " + student_name + " to the front. Their parent is here to pick them up.")
