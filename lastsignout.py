96d94c9640c1c95b3667768c80cb1a3caad8f2ed
Aahil Lakhani <lakhaniaahil@gmail.com>
2022-04-02 01:04:16 -0400

    revert compartmentalization

--- lastsignout.py
+++ lastsignout.py
@@ -14,25 +14,20 @@ def get_room_name(rooms, id):
         if room['id'] == id:
             return room['name']
 
-def get_lso_room_and_name():
-    # Would be call to Students Attendance with school_id as parameter
-    with open('students_attendance.json') as sa:
-        students_attendance = json.load(sa)['attendance']
-    student_id = get_last_sign_out(students_attendance)
+# Would be call to Students Attendance with school_id as parameter
+with open('students_attendance.json') as sa:
+    students_attendance = json.load(sa)['attendance']
+student_id = get_last_sign_out(students_attendance)
 
-    # Would be call to Student Detailed Info with student_id and school_id as parameter
-    with open(f'student_detailed_info/{student_id}.json') as sdi:
-        student_detailed_info = json.load(sdi)['student']
-    student_name = f"{student_detailed_info['first_name']} {student_detailed_info['last_name']}"
-    room_id = student_detailed_info['room_id']
+# Would be call to Student Detailed Info with student_id and school_id as parameter
+with open(f'student_detailed_info/{student_id}.json') as sdi:
+    student_detailed_info = json.load(sdi)['student']
+student_name = f"{student_detailed_info['first_name']} {student_detailed_info['last_name']}"
+room_id = student_detailed_info['room_id']
 
-    # Would be call to Rooms with school_id as parameter
-    with open('rooms.json') as r:
-        rooms = json.load(r)['rooms']
-    room_name = get_room_name(rooms, room_id)
+# Would be call to Rooms with school_id as parameter
+with open('rooms.json') as r:
+    rooms = json.load(r)['rooms']
+room_name = get_room_name(rooms, room_id)
 
-    return {'student_name': student_name, 'room_name': room_name}
-
-last_sign_out = get_lso_room_and_name()
-
-print(f"{last_sign_out['student_name']}: Please bring {last_sign_out['student_name']} to the front. Their parent is here to pick them up.")
+print(f"{room_name}: Please bring {student_name} to the front. Their parent is here to pick them up.")
